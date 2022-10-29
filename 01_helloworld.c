9a3f1c2b03c34729ed1440069f10eaabcfa36d7f
DiogoLeiteF <98144351+RelevantSilence@users.noreply.github.com>
2022-10-29 03:16:04 +0100

    1 entry

---
+++ 01_helloworld.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("I like pizza!\n");
+    printf("It's really good!\n");
+
+    return 0;
+}
+
+
+// gcc name_of_file.c -o name_of_exe
+// run exe file
+
+
+/*
+muti
+line 
+comments
+*/
+
