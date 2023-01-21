ae575494da1c61f37d0a975cdf76c003ac02d11c
DiogoLeiteF <diogoleitefaria@gmail.com>
2023-01-21 23:59:11 +0000

    c arrays

--- 8_arrays/challengeWeather.c
+++ 8_arrays/challengeWeather.c
@@ -1,69 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+# define MONTHS 12
+# define YEARS 5
+
 
 int main(int argc, char const *argv[])
 {
-    float rainfall[5][12] = {
+    double rainfall[YEARS][MONTHS] = {
         {7.3, 7.3, 4.9, 3.0, 2.3, 0.6, 1.2, 0.3, 0.5, 1.7, 3.6, 6.7},
-        {7.3, 7.3, 4.9, 3.0, 2.3, 0.6, 1.2, 0.3, 0.5, 1.7, 3.6, 6.7},
-        {7.3, 7.3, 4.9, 3.0, 2.3, 0.6, 1.2, 0.3, 0.5, 1.7, 3.6, 6.7},
-        {7.3, 7.3, 4.9, 3.0, 2.3, 0.6, 1.2, 0.3, 0.5, 1.7, 3.6, 6.7},
-        {7.3, 7.3, 4.9, 3.0, 2.3, 0.6, 1.2, 0.3, 0.5, 1.7, 3.6, 6.7},
+        {7.3, 7.3, 4.9, 3.0, 2.3, 0.6, 1.2, 0.3, 0.5, 1.7, 3.6, 5.7},
+        {7.3, 7.3, 4.9, 3.0, 2.3, 0.6, 1.2, 0.3, 0.5, 1.7, 3.6, 4.7},
+        {7.3, 7.3, 4.9, 3.0, 2.3, 0.6, 1.2, 0.3, 0.5, 1.7, 3.6, 3.7},
+        {7.3, 7.3, 4.9, 3.0, 2.3, 0.6, 1.2, 0.3, 0.5, 1.7, 3.6, 2.7},
       
     };    
+    double totalYear = 0;
+    double subTotal = 0;
+    
 
-    float rain10 = 0;
-    float rain11 = 0;
-    float rain12 = 0;
-    float rain13 = 0;
-    float rain14 = 0;
+    printf("YEAR:\tRainfall\n");
 
-    float month1;
-    float month2;
-    float month3;
-    float month4;
-    float month5;
-    float month6;
-    float month7;
-    float month8;
-    float month9;
-    float month10;
-    float month11;
-    float month12;
-
-    for (int i = 0; i < 12; i++)
-    {
-        rain10 += rainfall[0][i]; 
-        rain11 += rainfall[1][i]; 
-        rain12 += rainfall[2][i]; 
-        rain13 += rainfall[3][i]; 
-        rain14 += rainfall[4][i]; 
-    }
-
-    printf("\nYEAR\tRAINFALL");
-    printf("\n2010\t  %.1f", rain10);
-    printf("\n2011\t  %.1f", rain11);
-    printf("\n2012\t  %.1f", rain12);
-    printf("\n2013\t  %.1f", rain13);
-    printf("\n2014\t  %.1f", rain14);
-
-    for (int i = 0; i < 5; i++)
-    {
-        month1 += rainfall[i][0];
-        month2 += rainfall[i][1];
-        month3 += rainfall[i][2];
-        month4 += rainfall[i][3];
-        month5 += rainfall[i][4];
-        month6 += rainfall[i][5];
-        month7 += rainfall[i][6];
-        month8 += rainfall[i][7];
-        month9 += rainfall[i][8];
-        month10 += rainfall[i][9];
-        month11 += rainfall[i][10];
-        month12 += rainfall[i][11];
+    for (int i = 0; i < YEARS; i++){
+        for (int j = 0; j < MONTHS; j++)
+        {
+            totalYear += rainfall[i][j];
+        }
+        printf("%d\t%.1f\n", 2010 + i, subTotal);
+        totalYear += subTotal;
+        subTotal = 0;
     }
     
-    printf("\njan\tfeb\tmar\tapr\tmay\tjun\tjul\taug\tset\toct\tnov\tdec");
-    printf("\n%.1f\t%.1f\t%.1f\t%.1f\t%.1f\t%.1f\t%.1f\t%.1f\t%.1f\t%.1f\t%.1f\t%.1f\t", month1/12, month2/12, month3/12, month4/12, month5/12, month6/12, month7/12, month8/12, month9/12, month10/12, month11/12, month12/12);
+    printf("The yearly aaverage is %.2f in inches\n", subTotal/YEARS);
+
+    printf("\njan  feb  mar  apr  may  jun  jul  aug  sep  oct  nov  dec\n");
+    for (int i = 0; i < MONTHS; i++)
+    {
+        for (int j = 0; j < YEARS; j++)
+        {
+            subTotal += rainfall[j][i]; 
+        }
+        printf("%.1f  ", subTotal/YEARS);
+        subTotal = 0;
+    }
+    
 
 
 
